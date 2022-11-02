782a9f68105e6d0b422c11b96670ae11aef26072
vipzero <hiroflighter@gmail.com>
2022-11-02 23:50:19 +0900

    add rewrite

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewrites() {
+    return [{ source: "/schedule.jpg", destination: "/api/schedule" }]
+  },
 }
 
 module.exports = nextConfig
